d328025c4300a5ccf4918e8bd865e6db514746c2
Sravani521 <43287344+Sravani521@users.noreply.github.com>
2018-09-20 09:47:20 +0530

    jquery scripts

---
+++ 18September2018/18September2018/FirstJavaScript.js
@@ -0,0 +1,49 @@
+﻿alert("jquerycode is running");
+
+$("body").css("background", "orange");
+$(".header").css("color", "white");
+
+$(document).ready(FirstFun);
+$(document).ready(secondfun);
+
+
+function FirstFun()
+{
+    $("#head").css("background", "cyan");
+}
+
+
+function secondfun()
+{
+    $(".header").css("background", "violet");
+    //paragraph();
+}
+
+//function paragraph()
+//{
+//    alert($(".header").siblings());
+//}
+
+//$(document).ready
+//    (function ()
+//{
+//    $("p").click(function ()
+//    {
+//        $(this).hide();
+//    }
+//   )
+//}
+//)
+console.log($(".header").siblings());
+//$("div").fadeOut();
+
+var myDiv = $("div");
+myDiv.find("p");
+myDiv.children("p");
+
+$("div p");
+
+$("div").find("p>strong");
+
+
+
---
+++ DateOn19Sep2018/19SEP2018/EventScript.js
@@ -0,0 +1,95 @@
+﻿$(document).ready(function () {
+
+    $("button").click(function () {
+        $("#p01").fadeOut();
+        $("#p02").fadeOut("slow");
+        $("#div3").fadeOut(3000);
+    });
+
+
+    $("#btnsubmit").click(function () {
+       alert("button clicked");
+        $("form").animate({
+            left: '250px',
+            opacity: '0.5',
+            height: '150px',
+            width: '150px'
+        });
+        alert("button clicked");
+        $("txtusername").append("<b>Appended text</b>");
+        });
+    $("#btnchangetext").click(function () {
+        $("#p01").text("normal text").slideDown();
+
+    });
+
+    $("#btnchangehtml").click(function () {
+
+        $("#p02").html("<p>html text</p>")
+    });
+
+
+    $("btnremove").click(function () {
+        $("#div1").remove();
+    });
+
+    $("btnempty").click(function () {
+        $("#div2").empty();
+    })
+
+
+   
+});
+
+$(document).ready(function () {
+    $("#btncheck").click(function () {
+        var fname = $("#txtfname").val();
+        var lname = $("#txtlname").val();
+
+        if ((fname == "") && (lname == "")) {
+            alert("invalid credentials");
+        }
+        })
+
+});
+
+   
+
+
+
+
+
+    $("#p01").focus(function () {
+        $(this).css("background-color", "purple");
+    });
+
+
+    
+
+    //$("#btnsubmit").click(function () {
+    //    $("#txtusername").append("<h1>sravani</h1>");
+    //}) 
+
+
+
+
+
+     
+
+
+
+
+
+$(document).ready(MouseEnterEvent);
+function MouseEnterEvent()
+{
+    $("#txtusername").mouseenter(function () {
+        alert("plz entr user name");
+    });
+    
+}
+
+
+
+
+
